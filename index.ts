f677c8aa91cf9ba16d7f20f50bf736ec6151ba67
Scott Moss <willscottmoss@gmail.com>
2024-11-05 11:44:15 -0600

    done

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { runLLM } from './src/llm'
+import { addMessages, getMessages } from './src/memory'
 const userMessage = process.argv[2]
 
 if (!userMessage) {
@@ -7,6 +8,13 @@ if (!userMessage) {
   process.exit(1)
 }
 
-const response = await runLLM({ userMessage })
+await addMessages([{ role: 'user', content: userMessage }])
+const messages = await getMessages()
+
+const response = await runLLM({
+  messages,
+})
+
+await addMessages([{ role: 'assistant', content: response }])
 
 console.log(response)
--- src/llm.ts
+++ src/llm.ts
@@ -1,10 +1,11 @@
+import type { AIMessage } from '../types'
 import { openai } from './ai'
 
-export const runLLM = async ({ userMessage }: { userMessage: string }) => {
+export const runLLM = async ({ messages }: { messages: AIMessage[] }) => {
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     temperature: 0.1,
-    messages: [{ role: 'user', content: userMessage }],
+    messages,
   })
 
   return response.choices[0].message.content
