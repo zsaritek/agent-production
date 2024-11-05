0b3410472094e1007afff62217e5b10dc080a11f
Scott Moss <willscottmoss@gmail.com>
2024-11-05 16:06:22 -0600

    done

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import { runAgent } from './src/agent'
-import { z } from 'zod'
+import { tools } from './src/tools'
 
 const userMessage = process.argv[2]
 
@@ -9,12 +9,4 @@ if (!userMessage) {
   process.exit(1)
 }
 
-const weatherTool = {
-  name: 'get_weather',
-  description: `use this to get the weather.`,
-  parameters: z.object({
-    reasoning: z.string().describe('why did you pick this tool?'),
-  }),
-}
-
-await runAgent({ userMessage, tools: [weatherTool] })
+await runAgent({ userMessage, tools })
--- src/llm.ts
+++ src/llm.ts
@@ -1,7 +1,7 @@
 import type { AIMessage } from '../types'
 import { openai } from './ai'
 import { zodFunction } from 'openai/helpers/zod'
-
+import { systemPrompt } from './systemPrompt'
 export const runLLM = async ({
   messages,
   tools,
@@ -14,7 +14,7 @@ export const runLLM = async ({
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     temperature: 0.1,
-    messages,
+    messages: [{ role: 'system', content: systemPrompt }, ...messages],
     tools: formattedTools,
     tool_choice: 'auto',
     parallel_tool_calls: false,
